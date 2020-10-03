960dcb32b34d5275a0365aa5e0240bbfe6c440f1
Dioris Diaz <diorid@gmail.com>
2020-10-03 03:57:35 -0400

    homescreen created with products prices,images,numreviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import Header from "./componets/Header";
-import Footer from "./componets/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <h1>Welcome to The myShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
