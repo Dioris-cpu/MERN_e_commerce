9bf09067ed6fd933fd01e9fc7283efbc6eed5ca2
Dioris Diaz <diorid@gmail.com>
2020-10-04 02:48:38 -0400

    fetch data from the backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios"
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+      setProduct(data);
+    };
+    fetchProduct();
+  }, []);
   return (
     <>
       <Link className="btn btn-dark my-3" to="/">
@@ -46,14 +55,18 @@ const ProductScreen = ({ match }) => {
                 <Row>
                   <Col>Status:</Col>
                   <Col>
-                   {product.countInStock > 0 ? 'In Stock ' : 'Out Of Stock'}
+                    {product.countInStock > 0 ? "In Stock " : "Out Of Stock"}
                   </Col>
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                  <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
-                      Add to Cart
-                  </Button>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
               </ListGroup.Item>
             </ListGroup>
           </Card>
