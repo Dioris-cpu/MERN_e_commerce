4bbb43c4ea60181222e59d1f3514ff809e618d18
Dioris Diaz <diorid@gmail.com>
2020-10-25 14:46:46 -0400

    cartScreen bugs fixed

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -29,9 +29,15 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productId, qty]);
 
-  const removeFromCartHandler = (id) =>{
-      console.log()
+  const removeFromCartHandler = (id) => {
+    console.log();
+  };
+
+  const checkoutHandler = () =>{
+   history.push('/login?redirect=shipping')
+
   }
+
 
   return (
     <Row>
@@ -47,7 +53,7 @@ const CartScreen = ({ match, location, history }) => {
               <ListGroup key={item.product}>
                 <Row>
                   <Col md={2}>
-                    <Image src={item.Image} alt={item.name} fluid rounded />
+                    <Image src={item.image} alt={item.name} fluid rounded />
                   </Col>
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
@@ -56,8 +62,12 @@ const CartScreen = ({ match, location, history }) => {
                   <Col md={2}>
                     <Form.Control
                       as="select"
-                      value={qty}
-                      onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
                     >
                       {[...Array(item.countInStock).keys()].map((x) => (
                         <option key={x + 1} value={x + 1}>
@@ -67,9 +77,13 @@ const CartScreen = ({ match, location, history }) => {
                     </Form.Control>
                   </Col>
                   <Col md={2}>
-                      <Button type='button' variant= 'light' onClick={() => removeFromCartHandler(item.product)}>
-                          <i className='fas fa-trash'></i>
-                      </Button>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
                   </Col>
                 </Row>
               </ListGroup>
@@ -77,8 +91,30 @@ const CartScreen = ({ match, location, history }) => {
           </ListGroup>
         )}
       </Col>
-      <Col md={2}></Col>
-      <Col md={2}></Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items{" "}
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              > Proceed to checkout</Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
     </Row>
   );
 };
