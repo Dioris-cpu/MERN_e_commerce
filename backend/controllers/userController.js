f0ee1af87e46c7f8b9f59a506fe77d473b766224
Dioris Diaz <diorid@gmail.com>
2020-10-27 01:37:19 -0400

    auth users in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,6 +27,46 @@ const authUser = asyncHandler(async(req, res )  =>{
   }
 })
 
+
+
+// @description      Register a new user
+// @route            POST /api/users/
+// @access           Public
+
+const registerUser = asyncHandler(async(req, res )  =>{
+    const {name, email, password} = req.body
+ 
+   const userExists = await User.findOne({email})
+ 
+   if(userExists){
+       res.status(400)
+       throw new Error('user already exists')
+   }
+
+   const user = await User.create({
+       name,
+       email,
+       password
+   })
+   if(user){
+       res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id)
+       })
+   } else{
+       res.status(400)
+       throw new Error('User not found')
+   }
+ })
+
+
+
+
+
+
 // @description      Get user and profile 
 // @route            GET /api/users/profile
 // @access           private
@@ -48,4 +88,4 @@ const getUserProfile = asyncHandler(async(req, res )  =>{
  }
  })
 
-export { authUser, getUserProfile}
+export { authUser,  registerUser, getUserProfile }
