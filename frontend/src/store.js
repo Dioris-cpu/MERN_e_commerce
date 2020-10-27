9167225fb778ad2b02e5e9a0e68c70bdb272311e
Dioris Diaz <diorid@gmail.com>
2020-10-27 18:14:19 -0400

    login screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,33 +6,33 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import {userLoginReducer} from './reducers/userReducers'
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
-  userLogin: userLoginReducer
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
-  const userIfoFromStorage = localStorage.getItem("userIfo")
-  ? JSON.parse(localStorage.getItem("userIfo"))
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
-const initalState = {
-  cart: {cartItems:cartItemsFromStorage },
-  userLogin: {userInfo: userIfoFromStorage}
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
 
 const store = createStore(
   reducer,
-  initalState,
+  initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
