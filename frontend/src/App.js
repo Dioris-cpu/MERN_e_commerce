0e3ef28e1a774877ed630a3a72e14c3f9d07e1ce
Dioris Diaz <diorid@gmail.com>
2020-10-29 17:48:50 -0400

    payment screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+
 
 
 
@@ -22,7 +24,8 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-        <Route path='/shipping' component={ShippingScreen} />
+        <Route path='/shipping' component={PaymentScreen} />
+        <Route path='/payment' component={ShippingScreen} />
         <Route path='/login' component={LoginScreen} />
         <Route path='/register' component={RegisterScreen} />
         <Route path='/profile' component={ProfileScreen} />
