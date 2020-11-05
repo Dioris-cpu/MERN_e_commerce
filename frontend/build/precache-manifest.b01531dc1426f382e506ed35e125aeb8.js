db2a2c044fcc1c48c629d17d65064c3d96a047c2
Dioris Diaz <diorid@gmail.com>
2020-11-04 20:44:04 -0500

    getting ready to deploy app

---
+++ frontend/build/precache-manifest.b01531dc1426f382e506ed35e125aeb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d87a0fbfed3b885d71231a960309980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e24b18969e67fa81cc28",
+    "url": "/static/css/main.a370e806.chunk.css"
+  },
+  {
+    "revision": "d76406e46a8aef6432e2",
+    "url": "/static/js/2.ce8a17e7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.ce8a17e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24b18969e67fa81cc28",
+    "url": "/static/js/main.cfdd56c3.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
